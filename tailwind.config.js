2420615bf18a99310d32080cdca07e1af5ebb5b1
zahra mohammadi <zahramohammadi7294@gmail.com>
2024-07-09 15:49:34 +0330

    initial commit and products page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["vazir"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+      strategy: "class", // only generate classes
+    }),
+  ],
+};
